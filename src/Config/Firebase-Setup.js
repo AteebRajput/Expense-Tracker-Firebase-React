fadfd906a20554a4d630901aae4af817a6060053
Muhammad Ateeb Alam <muhammadateeb012@gmail.com>
2024-08-30 01:27:39 +0500

    Add Final Touches

--- src/Config/Firebase-Setup.js
+++ src/Config/Firebase-Setup.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPdKaL_VWI4eBbAHWBJvOVGylEUhrU564",
-  authDomain: "expense-tracker-6b3a6.firebaseapp.com",
-  projectId: "expense-tracker-6b3a6",
-  storageBucket: "expense-tracker-6b3a6.appspot.com",
-  messagingSenderId: "801994899843",
-  appId: "1:801994899843:web:b9632109d5b2fefff8f6b6",
-  measurementId: "G-16V01W5ME9"
+// Add your keys here
 };
 
 // Initialize Firebase
