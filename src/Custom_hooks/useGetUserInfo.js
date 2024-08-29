c6a4da0f88a00b7914066ea4f1afeb97d2ed2afc
Muhammad Ateeb Alam <muhammadateeb012@gmail.com>
2024-08-30 01:27:23 +0500

    Add Responsiveness and remove keys

--- src/Custom_hooks/useGetUserInfo.js
+++ src/Custom_hooks/useGetUserInfo.js
@@ -1,18 +1,25 @@
 export const useGetUser = () => {
-    const storedUser = localStorage.getItem('auth');
-    
-    if (!storedUser) {
-      return null;  // Return null if no user is found in localStorage
-    }
-  
-    const { uid, email, name, photo } = JSON.parse(storedUser);
-  
+  const storedUser = localStorage.getItem('auth');
+
+  if (!storedUser) {
     return {
-      userID: uid,
-      email,
-      name,
-      photoURL: photo,
-      isAuth : true
+      isAuth: false,
+      userID: null,
+      email: null,
+      name: null,
+      photoURL: null
     };
+  }
+
+  const { uid, email, name, photo } = JSON.parse(storedUser) || {};
+
+  return {
+    userID: uid,
+    email,
+    name,
+    photoURL: photo,
+    isAuth: true
   };
-export default useGetUser  
+};
+
+export default useGetUser;
