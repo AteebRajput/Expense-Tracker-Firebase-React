f75348c3201270035b0a9d52498ec4714478e00d
MUHAMMAD ATEEB ALAM <95118839+AteebRajput@users.noreply.github.com>
2025-02-13 01:09:02 +0500

    Update Firebase-Setup.js

--- src/Config/Firebase-Setup.js
+++ src/Config/Firebase-Setup.js
@@ -6,12 +6,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   // Add your keys here
-  apiKey: "AIzaSyCOr8Yj-pHn63UkcPxJgovP8YIuqe-Zq10",
-  authDomain: "expensetracker-154d2.firebaseapp.com",
-  projectId: "expensetracker-154d2",
-  storageBucket: "expensetracker-154d2.firebasestorage.app",
-  messagingSenderId: "876134036978",
-  appId: "1:876134036978:web:eec95a09f55418d5d0a76b",
+
 };
 
 // Initialize Firebase
